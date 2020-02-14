fd823a9f89b13403ddd1eb4ae8bf2af13edb7a3a
Jonas Ward Van den Bulcke <jonas.vandenbulcke@gmail.com>
2020-02-14 23:26:07 +0100

    TPMS Stable Implementation

--- src/spacemapping_curve/distance.py
+++ src/spacemapping_curve/distance.py
@@ -1,7 +1,7 @@
 import math
 import Rhino.Geometry as rg
 
-class FuckYouBernhardFunction:
+class SDF:
 
     X_SCALE = 5.0
     Y_SCALE = 5.0
@@ -11,19 +11,87 @@ class FuckYouBernhardFunction:
 
     GLOBAL_SCALE = 40.0
 
-    def __init__(self):
+    def __init__(self, x_scale = None, y_scale = None, z_scale = None, height = None, global_scale = None):
 
-        pass
+        if x_scale == None:
+
+            self.x_scale = SDF.X_SCALE
+
+        else:
+
+            self.x_scale = x_scale
+
+        if y_scale == None:
+
+            self.y_scale = SDF.Y_SCALE
+
+        else:
+
+            self.y_scale = y_scale
+
+        if z_scale == None:
+
+            self.z_scale = SDF.Z_SCALE
+
+        else:
+
+            self.z_scale = z_scale
+
+        if height == None:
+
+            self.height_scale = SDF.HEIGHT
+
+        else:
+
+            self.height = height
+
+        if global_scale == None:
+
+            self.global_scale = SDF.GLOBAL_SCALE
+
+        else:
+
+            self.global_scale = global_scale
+
+    def xyz_scale(self, pt):
+
+        x_val = pt[0] / self.x_scale
+        y_val = pt[1] / self.y_scale
+        z_val = (pt[2] - self.height) / self.z_scale
+
+        return x_val, y_val, z_val
+
+
+
+class Gyroid(SDF):
 
     def get_distance(self, pt):
 
-        x_val = pt[0] / FuckYouBernhardFunction.X_SCALE
-        y_val = pt[1] / FuckYouBernhardFunction.Y_SCALE
-        z_val = (pt[2] - FuckYouBernhardFunction.HEIGHT) / FuckYouBernhardFunction.Z_SCALE
+        x, y, z = self.xyz_scale(pt)
 
-        dis = math.sin(x_val)*math.cos(y_val)+math.sin(y_val)*math.cos(x_val)+math.sin(z_val)*math.cos(x_val)
+        dis = math.cos(x) + math.cos(y) + math.cos(z)
 
-        return dis * FuckYouBernhardFunction.GLOBAL_SCALE
+        return dis * self.global_scale
+
+class SchwarzP(SDF):
+
+    def get_distance(self, pt):
+
+        x, y, z = self.xyz_scale(pt)
+
+        dis = math.sin(z)*math.cos(y)+math.sin(y)*math.cos(x)+math.sin(z)*math.cos(x)
+
+        return dis * self.global_scale 
+
+class SchwarzD(SDF):
+
+    def get_distance(self, pt):
+
+        x, y, z = self.xyz_scale(pt)
+
+        dis = math.sin(z)*math.cos(y)+math.sin(y)*math.cos(x)+math.sin(z)*math.cos(x)
+
+        return dis * self.global_scale 
 
 class DistanceToCurve:
 
