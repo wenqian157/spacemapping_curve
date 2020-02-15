8efbf189f4bc858e3885d6f1d15d94aa88376b4c
Jonas Ward Van den Bulcke <jonas.vandenbulcke@gmail.com>
2020-02-15 02:29:39 +0100

    Bunch of TPMS implemented
    SchwarzG / Gyroid
    SchwarzD / Diamond
    SchwarzN / Neovious
    SchwarzP
    Lindinoid
    SchwarzW / iWP
    SchwarzPW / PWHybrid
    
    PinchShapes Class (super().__init__(..) not working in Rhino? )
    PinchG
    PinchD
    PinchP
    PinchW

--- src/spacemapping_curve/distance.py
+++ src/spacemapping_curve/distance.py
@@ -1,7 +1,7 @@
 import math
 import Rhino.Geometry as rg
 
-class SDF:
+class TPMS:
 
     X_SCALE = 5.0
     Y_SCALE = 5.0
@@ -15,7 +15,7 @@ class SDF:
 
         if x_scale == None:
 
-            self.x_scale = SDF.X_SCALE
+            self.x_scale = TPMS.X_SCALE
 
         else:
 
@@ -23,7 +23,7 @@ class SDF:
 
         if y_scale == None:
 
-            self.y_scale = SDF.Y_SCALE
+            self.y_scale = TPMS.Y_SCALE
 
         else:
 
@@ -31,7 +31,7 @@ class SDF:
 
         if z_scale == None:
 
-            self.z_scale = SDF.Z_SCALE
+            self.z_scale = TPMS.Z_SCALE
 
         else:
 
@@ -39,7 +39,7 @@ class SDF:
 
         if height == None:
 
-            self.height_scale = SDF.HEIGHT
+            self.height_scale = TPMS.HEIGHT
 
         else:
 
@@ -47,7 +47,7 @@ class SDF:
 
         if global_scale == None:
 
-            self.global_scale = SDF.GLOBAL_SCALE
+            self.global_scale = TPMS.GLOBAL_SCALE
 
         else:
 
@@ -61,9 +61,180 @@ class SDF:
 
         return x_val, y_val, z_val
 
+# class HelicoidCatenoid(TPMS):
+class HelicoidCatenoid:
 
+    X_SCALE = 5.0
+    Y_SCALE = 5.0
+    Z_SCALE = 5.0
 
-class Gyroid(SDF):
+    HEIGHT = 20.0
+
+    GLOBAL_SCALE = 40.0
+
+    RHO = 1.0
+    ALFA = 2.0
+
+    def __init__(self, x_scale = None, y_scale = None, z_scale = None, height = None, global_scale = None, rho = None, alfa = None):
+
+        # super().__init__(x_scale, y_scale, z_scale, height, global_scale)
+
+        if x_scale == None:
+
+            self.x_scale = TPMS.X_SCALE
+
+        else:
+
+            self.x_scale = x_scale
+
+        if y_scale == None:
+
+            self.y_scale = TPMS.Y_SCALE
+
+        else:
+
+            self.y_scale = y_scale
+
+        if z_scale == None:
+
+            self.z_scale = TPMS.Z_SCALE
+
+        else:
+
+            self.z_scale = z_scale
+
+        if height == None:
+
+            self.height_scale = TPMS.HEIGHT
+
+        else:
+
+            self.height = height
+
+        if global_scale == None:
+
+            self.global_scale = TPMS.GLOBAL_SCALE
+
+        else:
+
+            self.global_scale = global_scale
+
+        if rho == None:
+
+            self.rho = HelicoidCatenoid.RHO
+
+        else:
+
+            self.rho = rho
+
+        if alfa == None:
+
+            self.alfa = HelicoidCatenoid.ALFA
+
+        else:
+
+            self.alfa = alfa
+
+    def xyz_scale(self, pt):
+
+        x_val = pt[0] / self.x_scale
+        y_val = pt[1] / self.y_scale
+        z_val = (pt[2] - self.height) / self.z_scale
+
+        return x_val, y_val, z_val
+
+# class PinchShapes(TPMS):
+class PinchShapes:
+
+    X_SCALE = 5.0
+    Y_SCALE = 5.0
+    Z_SCALE = 5.0
+
+    HEIGHT = 20.0
+
+    GLOBAL_SCALE = 40.0
+
+    R = 10.0
+    S = -.5
+    T = -14.0
+
+    def __init__(self, x_scale = None, y_scale = None, z_scale = None, height = None, global_scale = None, r = None, s = None, t = None):
+
+        # super(PinchShapes, self).__init__(x_scale, y_scale, z_scale, height, global_scale)
+
+        if x_scale == None:
+
+            self.x_scale = TPMS.X_SCALE
+
+        else:
+
+            self.x_scale = x_scale
+
+        if y_scale == None:
+
+            self.y_scale = TPMS.Y_SCALE
+
+        else:
+
+            self.y_scale = y_scale
+
+        if z_scale == None:
+
+            self.z_scale = TPMS.Z_SCALE
+
+        else:
+
+            self.z_scale = z_scale
+
+        if height == None:
+
+            self.height_scale = TPMS.HEIGHT
+
+        else:
+
+            self.height = height
+
+        if global_scale == None:
+
+            self.global_scale = TPMS.GLOBAL_SCALE
+
+        else:
+
+            self.global_scale = global_scale
+
+        if r == None:
+
+            self.r = PinchShapes.R
+
+        else:
+
+            self.r = r
+
+        if s == None:
+
+            self.s = PinchShapes.S
+
+        else:
+
+            self.s = s
+
+        if t == None:
+
+            self.t = PinchShapes.T
+
+        else:
+
+            self.t = t
+
+    def xyz_scale(self, pt):
+
+        x_val = pt[0] / self.x_scale
+        y_val = pt[1] / self.y_scale
+        z_val = (pt[2] - self.height) / self.z_scale
+
+        return x_val, y_val, z_val
+
+class SchwarzG(TPMS):
 
     def get_distance(self, pt):
 
@@ -73,7 +244,11 @@ class Gyroid(SDF):
 
         return dis * self.global_scale
 
-class SchwarzP(SDF):
+class Gyroid(SchwarzG):
+
+    pass
+
+class SchwarzP(TPMS):
 
     def get_distance(self, pt):
 
@@ -81,17 +256,156 @@ class SchwarzP(SDF):
 
         dis = math.sin(z)*math.cos(y)+math.sin(y)*math.cos(x)+math.sin(z)*math.cos(x)
 
-        return dis * self.global_scale 
+        return dis * self.global_scale
 
-class SchwarzD(SDF):
+class SchwarzD(TPMS):
 
     def get_distance(self, pt):
 
         x, y, z = self.xyz_scale(pt)
 
-        dis = math.sin(z)*math.cos(y)+math.sin(y)*math.cos(x)+math.sin(z)*math.cos(x)
+        s_x, s_y, s_z = math.sin(x), math.sin(y), math.sin(z)
+        c_x, c_y, c_z = math.cos(x), math.cos(y), math.cos(z)
+
+        dis = s_x * s_y * s_z + s_x * c_y * c_z + c_x * s_y * c_z + c_x * c_y * s_z
+
+        return dis * self.global_scale
+
+class SchwarzN(TPMS):
+
+    def get_distance(self, pt):
+
+        x, y, z = self.xyz_scale(pt)
+        c_x, c_y, c_z = math.cos(x), math.cos(y), math.cos(z)
+
+        dis = 3 * (c_x + c_y + c_z) + 4 * (c_x * c_y * c_z)
+
+        return dis * self.global_scale
+
+class Neovius(TPMS):
+
+    pass
+
+class Lindinoid(TPMS):
+
+    def get_distance(self, pt):
+
+        x, y, z = self.xyz_scale(pt)
+
+        c_x, c_y, c_z = math.cos(x), math.cos(y), math.cos(z)
+        c_2x, c_2y, c_2z = math.cos(2 * x), math.cos(2 * y), math.cos(2 * z)
+        s_x, s_y, s_z = math.sin(x), math.sin(y), math.sin(z)
+        s_2x, s_2y, s_2z = math.sin(2 * x), math.sin(2 * y), math.sin(2 * z)
+
+        term_a = s_2x * c_y * s_z + s_2y * c_z * s_x + s_2z * c_x * s_y
+        term_b = c_2x * c_2y + c_2y * c_2z + c_2z * c_2x
+
+        dis = term_a * .5 - term_b * .5 + .15
+
+        return dis * self.global_scale
+
+class SchwarzW(TPMS):
+
+    def get_distance(self, pt):
+
+        x, y, z = self.xyz_scale(pt)
+
+        c_x, c_y, c_z = math.cos(x), math.cos(y), math.cos(z)
+
+        dis = c_x * c_y + c_y * c_z + c_z * c_x - c_x * c_y * c_z
 
         return dis * self.global_scale 
+
+class iWP(SchwarzW):
+
+    pass
+
+class SchwarzPW(TPMS):
+
+    def get_distance(self, pt):
+
+        x, y, z = self.xyz_scale(pt)
+
+        c_x, c_y, c_z = math.cos(x), math.cos(y), math.cos(z)
+
+        dis = 4 * (c_x * c_y + c_y * c_z + c_z * c_x) - 3 * c_x * c_y * c_z + 2.4
+
+        return dis * self.global_scale
+
+class PWHybrid(SchwarzPW):
+
+    pass
+
+class PinchG(PinchShapes):
+
+    def get_distance(self, pt):
+
+        x, y, z = self.xyz_scale(pt)
+
+        c_x, c_y, c_z = math.cos(x), math.cos(y), math.cos(z)
+        c_2x, c_2y, c_2z = math.cos(2 * x), math.cos(2 * y), math.cos(2 * z)
+
+        t1 = c_x*c_y + c_y*c_z + c_z*c_x
+        t2 = c_2x + c_2y + c_2z
+
+        dis = t1 * self.r + t2 * self.s + self.t
+
+        return dis * self.global_scale
+
+class PinchD(PinchShapes):
+
+    def get_distance(self, pt):
+
+        x, y, z = self.xyz_scale(pt)
+
+        s_x0, s_y0, s_z0 = math.sin(x - 0.78539816), math.sin(y - 0.78539816), math.sin(z - 0.78539816)
+        c_x0, c_y0, c_z0 = math.cos(x - 0.78539816), math.cos(y - 0.78539816), math.cos(z - 0.78539816)
+        c_4x, c_4y, c_4z = math.cos(4 * x), math.cos(4 * y), math.cos(4 * z)
+
+        t1 = s_x0 * s_y0 * s_z0 + s_x0 * c_y0 * c_z0 + c_x0 * s_y0 * c_z0 + c_x0 * c_y0 * s_z0
+        t2 = c_4x + c_4y + c_4z
+
+        dis = t1 * self.r + t2 * self.s + self.t
+
+        return dis * self.global_scale
+
+class PinchP(PinchShapes):
+
+    def get_distance(self, pt):
+
+        x, y, z = self.xyz_scale(pt)
+
+        s_x, s_y, s_z = math.sin(x), math.sin(y), math.sin(z)
+        c_x, c_y, c_z = math.cos(x), math.cos(y), math.cos(z)
+        c_2x, c_2y, c_2z = math.cos(2 * x), math.cos(2 * y), math.cos(2 * z)
+
+        t1 = c_x * s_y + c_y * s_z + c_z * s_x
+        t2 = c_2x * c_2y + c_2y * c_2z + c_2z * c_2x
+
+        dis = t1 * self.r + t2 * self.s + self.t
+
+        return dis * self.global_scale
+
+class PinchW(PinchShapes):
+
+    def get_distance(self, pt):
+
+        x, y, z = self.xyz_scale(pt)
+
+        c_x, c_y, c_z = math.cos(x), math.cos(y), math.cos(z)
+
+        t1 = c_x + c_y + c_z
+        t2 = c_x * c_y + c_y * c_z + c_z * c_x
+
+        dis = t1 * self.r + t2 * self.s + self.t
+
+        return dis * self.global_scale
+
+class Helicoid(HelicoidCatenoid):
+
+    def get_distance(self, pt):
+
+        x, y, z = self.xyz_scale(pt)
 
 class DistanceToCurve:
 
